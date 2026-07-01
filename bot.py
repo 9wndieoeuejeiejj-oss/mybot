bf07e38db1b58fd5f8aa2bf145be5aa5a52443ae
9wndieoeuejeiejj-oss <9wndieoeuejeiejj@gmail.com>
2026-07-01 11:44:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,20 @@
-from bale import Bot, Message
-import os
+from bale import Bot, Update, Message
 
-TOKEN = "715460300:CiPzvD38I1kbZ6yMJxu-KiuZ"
-bot = Bot(TOKEN)
+client = Bot(token="715460300:CiPzvD38I1kbZ6yMJxu-Kiu2nz8FYcFljjM")
 
-@bot.event
+@client.event
 async def on_ready():
-    print("ربات آنلاین شد!")
+    print(client.user.username, "is Ready!")
 
-@bot.event
+@client.event
+async def on_update(update: Update):
+    print(update.update_id)
+
+@client.event
 async def on_message(message: Message):
-    if not message.chat:
-        return
-    
-    text = message.content or ""
-    chat_id = message.chat.id
-    user_id = message.author.id
-    username = message.author.username or "کاربر"
+    if message.content == "/start":
+        await message.reply("Hi, from python-bale-bot to everyone!")
+        if message.chat.is_group_chat:
+            await message.reply("It's is a special Hi for groups!")
 
-    # سلام
-    if text == "سلام":
-        await bot.send_message(chat_id, f"سلام {username} جان! 😊")
-        return
-
-    # اگه @ داشت
-    if "@" in text and not text.startswith("/"):
-        admins = await bot.get_chat_administrators(chat_id)
-        is_admin = False
-        for admin in admins:
-            if admin.user.id == user_id:
-                is_admin = True
-                break
-        
-        if not is_admin:
-            await bot.send_message(chat_id, f"@{username} ❌ ارسال @ ممنوع!")
-            await bot.ban_chat_member(chat_id, user_id)
-            await bot.unban_chat_member(chat_id, user_id)
-            await bot.send_message(chat_id, f"🚫 {username} حذف شد!")
-
-if __name__ == "__main__":
-    bot.run()
+client.run()
