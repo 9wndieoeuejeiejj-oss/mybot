820fe3254cd5c1b1b27e4f51baafdb4ec31b47ba
9wndieoeuejeiejj-oss <9wndieoeuejeiejj@gmail.com>
2026-07-01 12:09:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
 from bale import Bot, Update, Message
+from flask import Flask
+import os
+import threading
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "ربات در حال اجراست!"
+
+# راه‌اندازی ربات در یک ترد جداگانه
 client = Bot(token="715460300:CiPzvD38I1kbZ6yMJxu-Kiu2nz8FYcFljjM")
 
 @client.event
@@ -17,4 +27,12 @@ async def on_message(message: Message):
         if message.chat.is_group_chat:
             await message.reply("It's is a special Hi for groups!")
 
-client.run()
+def run_bot():
+    client.run()
+
+if __name__ == "__main__":
+    # اجرای ربات در ترد جداگانه
+    bot_thread = threading.Thread(target=run_bot)
+    bot_thread.start()
+    # اجرای سرور Flask روی پورت ۱۰۰۰۰
+    app.run(host="0.0.0.0", port=10000)
